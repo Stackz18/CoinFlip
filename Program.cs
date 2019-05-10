1e82ea28ecd0c4cf2820c31041e662f8358c201c
Anthony Stackman <Anth.stackman@hotmail.co.uk>
2019-05-10 15:16:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets flip a coin 1000 times.");
+            FlippingCoin();
+        }
+
+        private static void FlippingCoin()
+        {
+            int numberOfFlips = 1000;
+            int headsCount = 0;
+            int tailsCount = 0;
+            Random random = new Random();
+            for (int i = 0; i < numberOfFlips; i++)
+            {
+                int coinFlip = random.Next(1, 3);
+                if (coinFlip == 1)
+                {
+                    headsCount++;
+                }
+                else
+                {
+                    tailsCount++;
+                }
+            }
+            Console.WriteLine("Number of coin flips: {0}", numberOfFlips);
+            Console.WriteLine("Number of heads: {0}", headsCount);
+            Console.WriteLine("Number of tails: {0}", tailsCount);
+            Console.ReadLine();
+        }
+    }
+}
